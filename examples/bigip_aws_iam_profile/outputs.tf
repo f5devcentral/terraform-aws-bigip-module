953f482c758fc1a9b9b65bbe60ca7857a07f6fb1
RavinderReddyF5 <r.chinthalapalli@f5.com>
2021-02-17 21:58:11 +0530

    updated outputs to remove tls key

--- examples/bigip_aws_iam_profile/outputs.tf
+++ examples/bigip_aws_iam_profile/outputs.tf
@@ -43,6 +43,6 @@ output public_addresses {
   value       = module.bigip.*.public_addresses
 }
 
-output tls_privatekey {
-  value = tls_private_key.example.private_key_pem
-}
+// output tls_privatekey {
+//   value = tls_private_key.example.private_key_pem
+// }
